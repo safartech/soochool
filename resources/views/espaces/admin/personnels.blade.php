70ce115248caa23d64ba63e6462a676abc69682b
Farid <samfarid.dev@gmail.com>
2019-04-09 10:52:38 +0000

    redesign personnel 1

--- resources/views/espaces/admin/personnels.blade.php
+++ resources/views/espaces/admin/personnels.blade.php
@@ -1,13 +1,11 @@
-@extends("default")
+@extends("templates.wrapper.modern")
 @section('css')
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/select2/css/select2.min.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/jquery.gritter/css/jquery.gritter.css') }}"/>
     <style>
         .select2{
             width: 100%;
-
         }
-
     </style>
 
 @endsection
@@ -15,6 +13,7 @@
 @section('js')
     <script src="{{ asset('assets/lib/select2/js/select2.min.js') }}" type="module"></script>
     <template id="Personnels">
+
 
 
         <div class="col-sm-12">
@@ -193,46 +192,66 @@
                 </div>
             </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">Professeurs
-                    <div class="tools"> <button data-toggle="modal" data-target="#form-bp1" type="button" class="btn btn-space btn-success  ">Ajouter</button><span class="icon mdi mdi-more-vert"></span></div>
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">All Bugs</h4>
+                    <a class="heading-elements-toggle"><i class="la la-ellipsis-h font-medium-3"></i></a>
+                    <div class="heading-elements">
+                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#form-bp1"><i class="ft-plus white"></i> Ajouter un membre</button>
+                        <span class="dropdown">
+                        <button id="btnSearchDrop1" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn btn-warning btn-sm dropdown-toggle dropdown-menu-right"><i class="ft-download white"></i></button>
+                        <span aria-labelledby="btnSearchDrop1" class="dropdown-menu mt-1 dropdown-menu-right">
+                            <a href="#" class="dropdown-item"><i class="la la-calendar"></i> Due Date</a>
+                            <a href="#" class="dropdown-item"><i class="la la-random"></i> Priority </a>
+                            <a href="#" class="dropdown-item"><i class="la la-bar-chart"></i> Progress</a>
+                            <a href="#" class="dropdown-item"><i class="la la-user"></i> Assign to</a>
+                        </span>
+                    </span>
+                        <button class="btn btn-success btn-sm"><i class="ft-settings white"></i></button>
+                    </div>
                 </div>
-                <div class="panel-body">
-                    <table class="table table-condensed table-hover table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th class="text-center">Nom</th>
+                <div class="card-content">
+                    <div class="card-body">
+                        <!-- Task List table -->
+                        <div class="table-responsive">
+                            <table class="table table-white-space table-bordered table-middle table-condensed table-hover table-stripped">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th class="text-center">Nom</th>
 
-                            <th class="text-center">Prenoms</th>
-                            <th class="text-center">Sexe</th>
-                            <th class="text-center">Diplome</th>
-                            <th class="text-center">Adresse</th>
-                            {{--<th class="text-center">Nom Complet</th>--}}
-                            <th class="text-center">Action</th>
+                                    <th class="text-center">Prenoms</th>
+                                    <th class="text-center">Sexe</th>
+                                    <th class="text-center">Diplome</th>
+                                    <th class="text-center">Adresse</th>
+                                    {{--<th class="text-center">Nom Complet</th>--}}
+                                    <th class="text-center">Action</th>
 
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="(personnel,i) in personnels">
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="(personnel,i) in personnels">
 
-                            <td class="text-center">@{{ i+1 }}</td>
+                                    <td class="text-center">@{{ i+1 }}</td>
 
-                            <td class="text-center">@{{ personnel.nom }}</td>
-                            <td class="text-center">@{{ personnel.prenoms }}</td>
-                            <td class="text-center">@{{ personnel.sexe }}</td>
-                            <td class="text-center">@{{ personnel.diplomes }}</td>
-                            <td class="text-center">@{{ personnel.adresse }}</td>
-                            {{--<td class="text-center">@{{ personnel.nom_complet    }}</td>--}}
-                            <td class="text-center">
-                                <a class="btn btn-info"  @click="showEditorModal(personnel)"  data-toggle="modal">Modifier</a>
-                                <a class="btn btn-danger" @click="showDeleteModal(personnel)">Supprimer</a>
+                                    <td class="text-center">@{{ personnel.nom }}</td>
+                                    <td class="text-center">@{{ personnel.prenoms }}</td>
+                                    <td class="text-center">@{{ personnel.sexe }}</td>
+                                    <td class="text-center">@{{ personnel.diplomes }}</td>
+                                    <td class="text-center">@{{ personnel.adresse }}</td>
+                                    {{--<td class="text-center">@{{ personnel.nom_complet    }}</td>--}}
+                                    <td class="text-center">
+                                        <a class="btn btn-info"  @click="showEditorModal(personnel)"  data-toggle="modal">Modifier</a>
+                                        <a class="btn btn-danger" @click="showDeleteModal(personnel)">Supprimer</a>
 
-                            </td>
-                        </tr>
+                                    </td>
+                                </tr>
 
-                        </tbody>
-                    </table>
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
                 </div>
             </div>
 
